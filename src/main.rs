fbf0ec12d231c4af1d7061ce76ca2610132dac0e
Hung <skellybg245@gmail.com>
2024-12-01 17:48:22 +1100

    implement day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::{fs::File, io::Read};
+
+mod days;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buf = String::new();
+
+    File::open("input/day01.txt")
+        .unwrap()
+        .read_to_string(&mut buf)
+        .unwrap();
+
+    days::day01::solve_pt2(&buf);
 }
