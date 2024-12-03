f7e7f6f3f7d89a8ad0f9f0a8821f3cd920bbf393
Tuan Hung Pham <tuanhung.pham@luxuryescapes.com>
2024-12-03 16:36:23 +1100

    should fold instead of using cell

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,5 +1,3 @@
-use std::cell::Cell;
-
 use regex::Regex;
 
 #[aoc(day3, part1)]
@@ -14,20 +12,21 @@ pub fn part1(input: &str) -> i32 {
 #[aoc(day3, part2)]
 pub fn part2(input: &str) -> i32 {
     let re = Regex::new(r"mul\((\d+),(\d+)\)|do\(\)()()|don't\(\)()()").unwrap();
-    let is_enabled = Cell::new(true);
 
-    re.captures_iter(input).map(|m| m.extract::<2>()).map(|(instruction, [l, r])| {
-        if instruction == "do()" {
-            is_enabled.replace(true);
-        } else if instruction == "don't()" {
-            is_enabled.replace(false);
-        }
-        (instruction, [l, r])
-    }).map(|(instruction, [l, r])| {
-        if is_enabled.get() && instruction != "do()" && instruction != "don't()" {
-            l.parse::<i32>().unwrap() * r.parse::<i32>().unwrap()
-        } else {
-            0
-        }
-    }).sum::<i32>()
+    re.captures_iter(input)
+        .map(|m| m.extract::<2>())
+        .fold((true, 0), |mut acc, (instruction, [l, r])| {
+            if instruction == "do()" {
+                acc.0 = true;
+            } else if instruction == "don't()" {
+                acc.0 = false;
+            }
+
+            if acc.0 && instruction != "do()" && instruction != "don't()" {
+                acc.1 += l.parse::<i32>().unwrap() * r.parse::<i32>().unwrap();
+            }
+
+            acc
+        })
+        .1
 }
