82a50d127e1d86aaf6df2be467501b1b47686ebc
Hung <skellybg245@gmail.com>
2024-12-05 16:35:48 +1100

    clean

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
-mod day6;
+pub mod day1;
+pub mod day2;
+pub mod day3;
+pub mod day4;
+pub mod day5;
+pub mod day6;
+
 extern crate aoc_runner;
 
 #[macro_use]
 extern crate aoc_runner_derive;
 
 pub mod unoptimised;
-
-pub mod day1;
-pub mod day2;
-pub mod day3;
-pub mod day4;
-pub mod day5;
 
 aoc_lib! { year = 2024 }
